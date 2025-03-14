9d6acf2da230d88b31eeebd6f666889cc8da218b
WillianRRP <willianrenato754@gmail.com>
2025-03-14 20:40:05 -0300

    update

---
+++ backend Pizzaria/src/services/user/CreateUserService.ts
@@ -0,0 +1,50 @@
+import prismaClient from '../../prisma'
+
+
+interface UserRequest{
+    name: string,
+    email: string,
+    password: string
+}
+
+
+class CreateUserService{
+    async execute({name, email, password}: UserRequest){
+ // Verificar se ele enviou um email
+
+ if(!email){
+    throw new Error("Email incorreto, tente outro")
+ }
+
+ // Verififcar se esse email já esta cadastrado 
+ const userAlreadyExists = await prismaClient.user.findFirst({
+    where:{
+        email: email
+    }
+ })
+
+ if(userAlreadyExists){
+    throw new Error('Este usuário já existe')
+ }
+
+ const user = await prismaClient.user.create({
+    data:{
+        name: name,
+        email: email,
+        password: password,
+    },
+    select:{
+        id: true,
+        name: true,
+        email: true,
+    }
+
+ })
+
+ 
+
+ return user
+    }
+}
+
+export {CreateUserService}
