df5fbfe36dae5fe1eaa017488b7c0147f73c96f5
WillianRRP <willianrenato754@gmail.com>
2025-03-14 21:32:42 -0300

    Logging in and generating JWT token

--- backend Pizzaria/src/services/user/AuthUserService.ts
+++ backend Pizzaria/src/services/user/AuthUserService.ts
@@ -1,6 +1,8 @@
 import prismaClient from "../../prisma";
 import { compare } from 'bcryptjs'
 
+import { sign } from "jsonwebtoken";
+
 
 interface AuthRequest{
     email: string,
@@ -28,9 +30,30 @@ async execute({email, password}: AuthRequest){
     }
 
 
+    // Se deu certo vai gerar um token
+
+    const token = sign(
+        {
+            name: user.name,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            subject: user.id,
+            expiresIn: '30d'
+        }
+    )
 
 
-    return{ ok: true}
+
+
+    return{ 
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        token: token
+
+    }
 
 }
 
