bb0c8ab77e996f80b783daeb057c2314cbb96c93
WillianRRP <willianrenato754@gmail.com>
2025-03-19 20:12:22 -0300

    creating the middleware

--- backend Pizzaria/src/routes.ts
+++ backend Pizzaria/src/routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import{ CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
+import{ DetailUserController } from './controllers/user/DetailUserController'
+
+import { isAuthenticated } from './middleware/isAuthenticated';
 
 const router = Router();
 
 // Rotas user
 router.post('/users', new CreateUserController().handle)
+
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me',isAuthenticated, new DetailUserController().handle)
 
 export { router }; // Certifique-se de exportar o router corretamente
